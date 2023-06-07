a2bb050b356941741d94126d0d4b74dc089004f5
tarun <cpatarun@gmail.com>
2023-06-07 20:16:28 +0800

    fix: lockfile issue in nextjs runner on linux

--- packages/nextjs/src/main/lock.ts
+++ packages/nextjs/src/main/lock.ts
@@ -1,16 +1,26 @@
+import exitHook from "exit-hook";
 import lockfile from "proper-lockfile";
 import {getConfig} from "@contly/core";
 
-export const getProcessLock = async () => {
+const lock = async () => {
   const {paths} = await getConfig();
+  const filePath = paths.files.temp.lock;
+  const options = {
+    lockfilePath: paths.dirs.lock,
+  };
+  // Lockfile default options
+  // stale: 10000,
+  // update: 10000 / 2,
+  // retries: 0,
+  await lockfile.lock(filePath, options);
+  exitHook(() => {
+    lockfile.unlockSync(filePath, options);
+  });
+};
+
+export const getProcessLock = async () => {
   try {
-    await lockfile.lock(paths.files.temp.lock, {
-      lockfilePath: paths.dirs.lock,
-    });
-    // Lockfile default options
-    // stale: 10000,
-    // update: 10000 / 2,
-    // retries: 0,
+    await lock();
     return true;
   } catch {
     return false;
