7fa993017b8633b615cd8f89343a2e4ae599d34b
tarun <cpatarun@gmail.com>
2023-06-08 21:32:56 +0800

    docs(changeset): better nextjs runner for linux, support for nested content directories

---
+++ .changeset/happy-spiders-leave.md
@@ -0,0 +1,8 @@
+---
+"@contly/nextjs": patch
+"@contly/core": patch
+"@contly/content": patch
+"@contly/core-compat": patch
+---
+
+better nextjs runner for linux, support for nested content directories
---
+++ packages/core/src/lib/fs.ts
@@ -0,0 +1,61 @@
+import fs from "fs-extra";
+import path from "node:path";
+
+export const pathResolve = (...args: string[]) => {
+  return normalizePath(path.resolve(...args));
+};
+export const fileRead = async (filePath: string) => {
+  return await fs.readFile(filePath, "utf8");
+};
+export const fileWrite = async (filePath: string, body: string) => {
+  return await fs.writeFile(filePath, body, "utf8");
+};
+export const pathExistsMulti = async (paths: string[]) => {
+  for (const p of paths) {
+    if (await fs.pathExists(p)) {
+      return p;
+    }
+  }
+  return null;
+};
+export const getPackageRoot = async (fsPath: string): Promise<string> => {
+  const stat = await fs.stat(fsPath);
+  if (stat.isDirectory()) {
+    const files = await fs.readdir(fsPath);
+    if (files.includes("package.json")) {
+      return fsPath;
+    }
+  }
+  return await getPackageRoot(path.dirname(fsPath));
+};
+export const fileReadFirstLine = (filePath: string) =>
+  new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(filePath, {encoding: "utf8"});
+
+    let firstLine = "";
+    let foundLineEnd = false;
+
+    readStream.on("data", (chunk) => {
+      const data = Buffer.isBuffer(chunk) ? chunk.toString("utf-8") : chunk;
+      if (foundLineEnd) {
+        readStream.destroy();
+        return;
+      }
+
+      const lines = data.split(/\r?\n/);
+      firstLine += lines[0];
+
+      if (lines.length > 1) {
+        foundLineEnd = true;
+        readStream.destroy();
+      }
+    });
+
+    readStream.on("close", () => {
+      resolve(firstLine);
+    });
+
+    readStream.on("error", (err) => {
+      reject(err);
+    });
+  });
